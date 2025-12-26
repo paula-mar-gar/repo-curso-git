eedf9bf72a11e8eb40c9100ed7592ab5396024eb
Paula Martínez García <70705399+paula-mar-gar@users.noreply.github.com>
2025-12-26 15:24:46 +0100

    Update codigo.py
    Appellidos en dos variables separadas

--- codigo.py
+++ codigo.py
@@ -1,5 +1,7 @@
 nombre = "Alba"
-apellidos = "Martínez García"
+apellido1 = "Martínez"
+apellido2 = "García"
+apellidos = apellido1 + " " + apellido2
 
 saludo = "Hola "+ nombre+ " "+ apellidos + " ¿Cómo estás?"
 print(saludo)
